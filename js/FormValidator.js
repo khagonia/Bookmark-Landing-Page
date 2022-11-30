fa1b024890716ad88b0476c518cee9ea45ab53a6
Kaiser <dev.khagonia@gmail.com>
2022-12-01 02:48:26 +0800

    EOD commit
    Completed the Form Validation Script

---
+++ js/FormValidator.js
@@ -0,0 +1,49 @@
+export default class FormValidator {
+  constructor(form_selector, formHandler) {
+
+    this.formHandler = formHandler
+    this.form = document.querySelector(form_selector)
+    this.inputsWithErrors = new Set()
+
+    // console.log(this.form)
+    this.form.addEventListener('submit', e => {
+      e.preventDefault()      
+      this.formHandler(this.form,this.hasErrors)
+    })
+  }
+
+  get hasErrors() {
+    return this.inputsWithErrors.size > 0;
+  }
+
+  register(field_selector, check, error_element) {
+    const inputField = this.form.querySelector(field_selector)
+    const errorElement = error_element
+
+    const execute = (hideErrors) => {
+      const {pass, error} = check(inputField.value, inputField)
+
+      //do not execute on load
+
+      
+      if(!hideErrors) {
+        errorElement.textContent = error || ''
+        if(!pass) {
+          this.form.classList.add('error')
+        } else {
+          this.form.classList.remove('error') 
+        }
+      }
+
+      // execute on-loadprevent submit if it has errors
+      if(!pass) {
+        this.inputsWithErrors.add(inputField)
+      } else {
+        this.inputsWithErrors.delete(inputField)
+      }
+    }
+
+    inputField.addEventListener('change', () => execute())
+    execute(true)
+  }
+}
--- js/script.js
+++ js/script.js
@@ -3,30 +3,104 @@
 //////////////////////////
 
 const tabs = document.querySelectorAll(".feature-tab")
+const info = document.querySelectorAll(".feature-info")
+
+function removeActive(elements) {
+  elements.forEach((element) => {
+    element.classList.remove('active')
+  })
+}
 
 tabs.forEach((tab) => {
-  const tab1 = document.querySelector("#tab-1")
-  const tab2 = document.querySelector("#tab-2")
-  const tab3 = document.querySelector("#tab-3")
-
   tab.addEventListener('click', () => {
-    if(tab.classList.contains('tab1')) {
+    removeActive(tabs)
+    tab.classList.add('active')
 
-      tab1.classList.add('active')
-      tab2.classList.remove('active')
-      tab3.classList.remove('active')
+    removeActive(info)
+    if(tab.classList.contains("tab1")) {
+      document.querySelector('#tab-1').classList.add('active')
+    }
+
+    if(tab.classList.contains("tab2")) {
+      document.querySelector('#tab-2').classList.add('active')
     }
     
-    if(tab.classList.contains('tab2')) {
-      tab1.classList.remove('active')
-      tab2.classList.add('active')
-      tab3.classList.remove('active')
-    }
-
-    if(tab.classList.contains('tab3')) {
-      tab1.classList.remove('active')
-      tab2.classList.remove('active')
-      tab3.classList.add('active')
+    if(tab.classList.contains("tab3")) {
+      document.querySelector('#tab-3').classList.add('active')
     }
   })
 })
+
+//////////////////////////
+// FAQ ACCORDION
+//////////////////////////
+
+const questions = document.querySelectorAll('.question')
+
+questions.forEach((question) => {
+  question.addEventListener('click', () => {
+    question.closest('.question-box').classList.toggle('active')
+  })  
+})
+  
+//////////////////////////
+// FORM VALIDATION
+//////////////////////////
+
+import FormValidator from "./FormValidator.js"
+
+function check(value, inputField) {
+  if(value.length == '') {
+    return {
+      pass: false,
+      error: "Yikes don't leave this empty"
+    }
+  }
+
+  const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+  if(!pattern.test(value)) {
+    return {
+      pass: false,
+      error: "Whoops, make sure it's an email"
+    }
+  } 
+  
+  return {
+    pass: true,
+  }
+}
+
+function fHandler(form, hasErrors) {
+  if(!hasErrors) {
+    form.submit()
+  } else {
+    const inputField = form.querySelector('#email')
+    const errorElement = form.querySelector('.error-message')
+
+    const {pass, error} = check(inputField.value, inputField) 
+    form.querySelector('.input-container').classList.add('error')
+    form.querySelector('p').textContent = error
+  }      
+}
+
+const fv = new FormValidator('#cta-form', fHandler)
+
+// register(inputSelector, check): register an input field and a check logic
+//
+// check is a function which defines the
+// validation logic of an input field. Must return an object with the pass
+// result and its corresponding error message when it fails.
+//
+// in this case, if the input is empty... then an error message is thrown
+
+fv.register("#email", check, document.querySelector('.error-message'))
+
+
+
+// const input = document.querySelector('#email')
+// const inputBox = document.querySelector('.input-container')
+// inputBox.addEventListener('click', () => {
+//   inputBox.classList.remove('error')
+// })
+
